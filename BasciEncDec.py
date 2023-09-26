d8a9b835a0e1f15940b92ca6dcb03ed2b4f081e2
Ram Murthy Raju Vatsavai <rammurthyrajuvatsavai@gmail.com>
2023-09-26 15:30:10 +0530

    initial commit

---
+++ BasciEncDec.py
@@ -0,0 +1,25 @@
+def encrypt(msg, key):
+  msg = list(msg)
+  for i in range(len(msg)):
+    msg[i] = chr(ord(msg[i]) + i + key)
+  return "".join(msg)
+
+
+def decrypt(msg, key):
+  msg = list(msg)
+  for i in range(len(msg)):
+    msg[i] = chr(ord(msg[i]) - i - key)
+  return "".join(msg)
+
+
+s = str(input("enter the message: "))
+n = len(s)
+key = input("enter key: ")
+k = 0
+for i in key:
+  k += ord(i)
+
+enc_msg = encrypt(s, k)
+print("encrypted msg is \n" + enc_msg + "\n")
+dec_msg = decrypt(enc_msg, k)
+print("decrypted msg is \n" + dec_msg)
